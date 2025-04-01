5c35aabc6cab05427218a7eb925089eb4304198f
Elia Palme <2025999+theely@users.noreply.github.com>
2025-04-01 19:59:10 +0200

    Update swagger-initializer.js

--- openapi/dist/swagger-initializer.js
+++ openapi/dist/swagger-initializer.js
@@ -6,8 +6,6 @@ window.onload = function() {
     urls: [
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-latest.yaml", name: "Latest"},
       //add-newones-here
-      {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-master.yaml", name: "master"},
-      {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.2.3.yaml", name: "2.2.3"},
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.2.3.yaml", name: "2.2.3"},
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.2.2.yaml", name: "2.2.2"},
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.2.1.yaml", name: "2.2.1"},
