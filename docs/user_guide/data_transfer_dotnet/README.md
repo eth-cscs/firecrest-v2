b186d2f6c8b641f7cf2445462e5950dea1acdf47
Ivano Bonesana <ivano.bonesana@gmail.com>
2025-04-20 21:06:38 +0200

    Examples: firecrest_base and large files transfer (upload+download) Set default max_part_size to 1GB

---
+++ docs/user_guide/data_transfer_dotnet/README.md
@@ -0,0 +1,3 @@
+dotnet add package Microsoft.IdentityModel.JsonWebTokens --version 8.8.0
+
+NuGet\Install-Package Microsoft.IdentityModel.JsonWebTokens -Version 8.8.0
---
+++ docs/user_guide/data_transfer_dotnet/firecrest_base/SchedulerJobs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firecrest_base
+{
+    public class SchedulerJobs
+    {
+        public SchedulerJob []? jobs { get; set; }
+    }
+}
