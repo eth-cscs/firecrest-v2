812010d6ef705610c1f7fcb7bb1d4d6fd8d0bfae
theely <theely@users.noreply.github.com>
2025-06-06 13:58:46 +0000

    New firecrest-api openAPI doc release (2.2.7)

--- openapi/dist/swagger-initializer.js
+++ openapi/dist/swagger-initializer.js
@@ -6,6 +6,7 @@ window.onload = function() {
     urls: [
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-latest.yaml", name: "Latest"},
       //add-newones-here
+      {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-.yaml", name: ""},
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.2.7.yaml", name: "2.2.7"},
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.2.6.yaml", name: "2.2.6"},
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.2.5.yaml", name: "2.2.5"},
