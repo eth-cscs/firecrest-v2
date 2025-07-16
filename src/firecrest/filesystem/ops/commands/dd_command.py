87745e3b66c8c364dafb1f54c37503a22e39cf59
jdorsch <jdorsch@cscs.ch>
2025-07-16 09:12:44 +0200

    comment on why count=2 on dd

--- src/firecrest/filesystem/ops/commands/dd_command.py
+++ src/firecrest/filesystem/ops/commands/dd_command.py
@@ -23,29 +23,25 @@ class DdCommand(BaseCommandWithTimeout):
 
         self.target_path = target_path
         self.size = OPS_SIZE_LIMIT if (size is None or size > OPS_SIZE_LIMIT) else size
-        self.count = 1
         self.offset = offset
 
         if offset is None:
             self.skip = 0
             self.offset = 0
-        elif offset % self.size == 0:
-            self.skip = offset // self.size
         else:
-            self.count = 2
             self.skip = offset // self.size
 
     def get_command(self) -> str:
+        # `count = 2` to bring back 2 chunks of the file, in case the offset is not a multiple of `size`
+        # After, the `stdout` is processed on the `parse_output` method to return the requested chunk
+        # This increases efficiency by not bringing back `bs` of size 1B which leads to multiple reads (`count=N`) on the system
         return (
-            f"{super().get_command()} dd if='{self.target_path}' bs={self.size} skip={self.skip} count={self.count}"
+            f"{super().get_command()} dd if='{self.target_path}' bs={self.size} skip={self.skip} count=2"
         )
 
     def parse_output(self, stdout: str, stderr: str, exit_status: int):
         if exit_status != 0:
             super().error_handling(stderr, exit_status)
 
-        i = 0
-        if self.count == 2:
-            i = self.offset % self.size
-
+        i = self.offset % self.size
         return stdout[i:i+self.size]
