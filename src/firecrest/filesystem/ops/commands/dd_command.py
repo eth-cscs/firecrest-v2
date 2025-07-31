aae7435d216aad6ee2e31692895aa0ee5b575369
Elia Palme <elia.palme@cscs.ch>
2025-07-31 17:43:07 +0200

    fix dd when storage setting is not defined

--- src/firecrest/filesystem/ops/commands/dd_command.py
+++ src/firecrest/filesystem/ops/commands/dd_command.py
@@ -11,14 +11,16 @@ from firecrest.filesystem.ops.commands.base_command_with_timeout import (
     BaseCommandWithTimeout,
 )
 
-
-OPS_SIZE_LIMIT = settings.storage.max_ops_file_size
+OPS_SIZE_LIMIT = 5 * 1024 * 1024
+if settings.storage:
+    OPS_SIZE_LIMIT = settings.storage.max_ops_file_size
 
 
 class DdCommand(BaseCommandWithTimeout):
 
-    def __init__(self, target_path: str = None, size: int = None,
-                 offset: int = 0) -> None:
+    def __init__(
+        self, target_path: str = None, size: int = None, offset: int = 0
+    ) -> None:
         super().__init__()
 
         self.target_path = target_path
@@ -35,13 +37,11 @@ class DdCommand(BaseCommandWithTimeout):
         # `count = 2` to bring back 2 chunks of the file, in case the offset is not a multiple of `size`
         # After, the `stdout` is processed on the `parse_output` method to return the requested chunk
         # This increases efficiency by not bringing back `bs` of size 1B which leads to multiple reads (`count=N`) on the system
-        return (
-            f"{super().get_command()} dd if='{self.target_path}' bs={self.size} skip={self.skip} count=2"
-        )
+        return f"{super().get_command()} dd if='{self.target_path}' bs={self.size} skip={self.skip} count=2"
 
     def parse_output(self, stdout: str, stderr: str, exit_status: int):
         if exit_status != 0:
             super().error_handling(stderr, exit_status)
 
         i = self.offset % self.size
-        return stdout[i:i+self.size]
+        return stdout[i : i + self.size]
