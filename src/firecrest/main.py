5931f2afb9866337e4c097762911e9d0d445c886
Ivano Bonesana <ivano.bonesana@cscs.ch>
2026-01-19 14:47:42 +0100

    managed missing probing_services

--- src/firecrest/main.py
+++ src/firecrest/main.py
@@ -116,11 +116,12 @@ async def lifespan(app: FastAPI):
 async def schedule_tasks(scheduler: AsyncScheduler):
     for cluster in plugin_settings.clusters:
         if cluster.probing_services:
-            await scheduler.add_schedule(
-                ClusterHealthChecker(cluster).check,
-                IntervalTrigger(seconds=cluster.probing_services.interval_check),
-                id=f"check-cluster-{cluster.name}",
-            )
+            if cluster.probing_services.services:
+                await scheduler.add_schedule(
+                    ClusterHealthChecker(cluster).check,
+                    IntervalTrigger(seconds=cluster.probing_services.interval_check),
+                    id=f"check-cluster-{cluster.name}",
+                )
     if (
         settings.data_operation.data_transfer
         and settings.data_operation.data_transfer.probing
--- src/lib/loggers/tracing_log.py
+++ src/lib/loggers/tracing_log.py
@@ -88,15 +88,8 @@ def tracing_log_middleware(request: Request, username: str, status_code: int) ->
     log_data["endpoint"] = endpoint
     log_data["resource"] = resource
     log_data["status_code"] = status_code
-    try: 
-        log_data["user_agent"] = request.headers["user-agent"]
-        log_data["x_log_application_id"] = request.headers["x-log-application-id"]
-        log_data["x_log_application_name"] = request.headers["x-application-name"]
-        log_data["x_log_client"] = request.headers["x-log-client"]
-        log_data["x-log_api_publisher"] = request.headers["x-log-api-publisher"]
-    except Exception as e:
-        print("Header exception "+str(e))
-        print(request.headers)
+    log_data["user_agent"] = request.headers["user-agent"]
+
     # Get backend log if any
     backend = get_tracing_backend_log()
     if backend is not None:
