f0315b9ebeba1ff2c8def6bcc8e351a2f2cecb6a
Elia Palme <elia.palme@cscs.ch>
2026-05-26 18:14:37 +0200

    add shlex to secure the commands execution

--- src/lib/scheduler_clients/slurm/cli_commands/sbatch_command.py
+++ src/lib/scheduler_clients/slurm/cli_commands/sbatch_command.py
@@ -5,6 +5,7 @@
 
 # commands
 import re
+import shlex
 from lib.exceptions import SlurmError
 
 from lib.scheduler_clients.models import JobDescriptionModel
@@ -22,26 +23,26 @@ class SbatchCommand(BaseCommand):
         env = ",".join(
             f"{key}={value}" for key, value in self.job_description.environment.items()
         )
-        cmd += [f"--export='ALL,{env}'"]
-        cmd += [f"--chdir='{self.job_description.current_working_directory}'"]
+        cmd += [f"--export={shlex.quote(f'ALL,{env}')}"]
+        cmd += [f"--chdir={shlex.quote(self.job_description.current_working_directory)}"]
         if self.job_description.partition:
-            cmd.append(f"--partition='{self.job_description.partition}'")
+            cmd.append(f"--partition={shlex.quote(self.job_description.partition)}")
         if self.job_description.reservation:
-            cmd.append(f"--reservation='{self.job_description.reservation}'")
+            cmd.append(f"--reservation={shlex.quote(self.job_description.reservation)}")
         if self.job_description.account:
-            cmd.append(f"--account='{self.job_description.account}'")
+            cmd.append(f"--account={shlex.quote(self.job_description.account)}")
         if self.job_description.name:
-            cmd.append(f"--job-name='{self.job_description.name}'")
+            cmd.append(f"--job-name={shlex.quote(self.job_description.name)}")
         if self.job_description.standard_error:
-            cmd += [f"--error='{self.job_description.standard_error}'"]
+            cmd += [f"--error={shlex.quote(self.job_description.standard_error)}"]
         if self.job_description.standard_output:
-            cmd += [f"--output='{self.job_description.standard_output}'"]
+            cmd += [f"--output={shlex.quote(self.job_description.standard_output)}"]
         if self.job_description.standard_input:
-            cmd += [f"--input='{self.job_description.standard_input}'"]
+            cmd += [f"--input={shlex.quote(self.job_description.standard_input)}"]
         if self.job_description.constraints:
-            cmd.append(f"--constraint='{self.job_description.constraints}'")
+            cmd.append(f"--constraint={shlex.quote(self.job_description.constraints)}")
         if self.job_description.script_path:
-            cmd.append(f" -- {self.job_description.script_path}")
+            cmd.append(f"-- {shlex.quote(self.job_description.script_path)}")
         return " ".join(cmd)
 
     def parse_output(self, stdout: str, stderr: str, exit_status: int = 0):
