12d2f552b6324b3d39f6854fff3ddab684e2ab79
jpdorsch <jpdorsch@users.noreply.github.com>
2026-08-25 15:36:37 +0000

    New firecrest-api openAPI doc release (2.6.0)

--- openapi/dist/swagger-initializer.js
+++ openapi/dist/swagger-initializer.js
@@ -6,6 +6,7 @@ window.onload = function() {
     urls: [
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-latest.yaml", name: "Latest"},
       //add-newones-here
+      {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.6.0.yaml", name: "2.6.0"},
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.5.6.yaml", name: "2.5.6"},
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.5.6.yaml", name: "2.5.6"},
       {url: "https://eth-cscs.github.io/firecrest-v2/openapi/openapi-2.5.6.yaml", name: "2.5.6"},
